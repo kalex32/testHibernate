acf92a98aec926385a96240d2489ef4fe69af589
Olexiy Koval <kalex32@ukr.net>
2019-01-23 13:27:48 +0200

    Created User and Auto classes

--- src/main/java/models/Auto.java
+++ src/main/java/models/Auto.java
@@ -1,4 +1,64 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "autos")
 public class Auto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_autos;
+
+    @Column (name = "model_autos")
+    private String model_autos;
+
+    //можно не указывать Column name, если оно совпадает с названием столбца в таблице
+    private String color_autos;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Auto() {
+    }
+
+    public Auto(String model_autos, String color_autos) {
+        this.model_autos = model_autos;
+        this.color_autos = color_autos;
+    }
+
+    public int getId_autos() {
+        return id_autos;
+    }
+
+    public String getModel_autos() {
+        return model_autos;
+    }
+
+    public void setModel_autos(String model_autos) {
+        this.model_autos = model_autos;
+    }
+
+    public String getColor_autos() {
+        return color_autos;
+    }
+
+    public void setColor_autos(String color_autos) {
+        this.color_autos = color_autos;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return color_autos + " " + model_autos;
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,77 @@
 package models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table (name = "users")
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_users;
+    @Column(name = "name_users")
+    private String name_users;
+    //можно не указывать Column name_users, если оно совпадает с названием столбца в таблице
+    private int age_users;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Auto> autos;
+
+    public User() {
+    }
+
+    public User(String name_users, int age_users) {
+        this.name_users = name_users;
+        this.age_users = age_users;
+        autos = new ArrayList<>();
+    }
+
+    public void addAuto(Auto auto) {
+        auto.setUser(this);
+        autos.add(auto);
+    }
+
+    public void removeAuto(Auto auto) {
+        autos.remove(auto);
+    }
+
+    public int getId_users() {
+        return id_users;
+    }
+
+    public String getName_users() {
+        return name_users;
+    }
+
+    public void setName_users(String name_users) {
+        this.name_users = name_users;
+    }
+
+    public int getAge_users() {
+        return age_users;
+    }
+
+    public void setAge_users(int age_users) {
+        this.age_users = age_users;
+    }
+
+    public List<Auto> getAutos() {
+        return autos;
+    }
+
+    public void setAutos(List<Auto> autos) {
+        this.autos = autos;
+    }
+
+    @Override
+    public String toString() {
+        return "models.User{" +
+                "id_users=" + id_users +
+                ", name_users='" + name_users + '\'' +
+                ", age_users=" + age_users +
+                '}';
+    }
 }
